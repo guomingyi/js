8dcf6f3eb1bb1577b4763a3b1588bf579ba7dcd2
gmy <296778322@qq.com>
2018-07-26 11:52:30 +0800

    <http server>

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 var hello = require('./hello'); 
+var server = require('./server'); // add external module require.
 
 function buffer_test() {
 	const buf = Buffer.from('abcd', 'ascii');
@@ -13,6 +14,7 @@ hello.hello()
 
 buffer_test()
 
+server.http_server() // call external function.
 
 
 
---
+++ server.js
@@ -0,0 +1,30 @@
+var http = require('http');
+
+function http_server_test() {
+
+	http.createServer(function (request, response) {
+	    // 发送 HTTP 头部 
+	    // HTTP 状态值: 200 : OK
+	    // 内容类型: text/plain
+	    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+	    // 发送响应数据 "Hello World"
+	    response.end('Hello World!\n');
+	}).listen(8888);
+
+	console.log('Server running at http://127.0.0.1:8888/');
+}
+
+//http_server_test()
+
+exports.http_server = http_server_test;
+
+
+
+
+
+
+
+
+
+
