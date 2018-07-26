0901e74a40044d177b15f0a3f9fee1f5c0889560
gmy <296778322@qq.com>
2018-07-26 11:35:36 +0800

    <js for hello world>

---
+++ hello.js
@@ -0,0 +1,34 @@
+
+function sayHelloWorld(){
+    document.write("Hello world#"); // nodejs not support this function
+    console.log("hello world!");
+}
+
+sayHelloWorld()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
